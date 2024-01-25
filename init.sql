f628c44be59f32f289bd24b6f720036959a1b900
SAKURUG山﨑 <yamazaki@sakurug.co.jp>
2024-01-25 18:48:51 +0900

    add:docker

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE ROLE todo_user LOGIN PASSWORD 'todo_pass';
+CREATE DATABASE todo_db WITH OWNER = todo_user;
+\c todo_db todo_user
+CREATE TABLE todo_items (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(40),
+    done_flg NUMERIC(1) DEFAULT 0,
+    time_limit DATE
+);
+ALTER TABLE todo_items OWNER TO todo_user;
